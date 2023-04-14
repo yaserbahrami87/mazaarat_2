756ac01a068e1780cd31783fc711833696c6efea
Yaser Bahrami <yaser.bahrami@gmail.com>
2023-04-14 21:19:45 +0430

    start back mazaarat

---
+++ resources/views/farsi/gallery.blade.php
@@ -0,0 +1,172 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="/css/bootstrap.min.css" />
+    <link href="/css/bootstrap-rtl.min.css" rel="stylesheet" />
+    <link href="/css/style.css" rel="stylesheet" />
+    <title></title>
+</head>
+<body>
+<main class="container-fluid" id="main_gallery" dir="rtl">
+    <nav class="navbar navbar-expand-lg navbar-light bg-transparent mb-5">
+        <button class="navbar-toggler bg-light" type="button" data-toggle="collapse" data-target="#navbarVerticalContent" aria-controls="navbarVerticalContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarVerticalContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link text-light" href="#">ثبت نام <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light" href="#">ارسال عکس</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light" href="#">گالری</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light" href="#">اخبار</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light" href="#">فراخوان</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light" href="#">ارکان جشنواره</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light" href="#">مدیریت</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light" href="#">داوران</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light" href="/">خانه</a>
+                </li>
+            </ul>
+
+        </div>
+    </nav>
+
+    <div class="container">
+        <div class="row align-items-center mt-3 mb-3" id="gallery">
+            <div class="col-6 col-md-3 mb-4 box_image">
+                <a href="">
+                    <img src="/images/gallery/1.jpg" class="img-fluid"  />
+                </a>
+
+            </div>
+            <div class="col-6 col-md-3 mb-4 box_image">
+                <a href="">
+                    <img src="/images/gallery/1.jpg" class="img-fluid"  />
+                </a>
+
+            </div>
+            <div class="col-6 col-md-3 mb-4 box_image">
+                <a href="">
+                    <img src="/images/gallery/1.jpg" class="img-fluid"  />
+                </a>
+
+            </div>
+            <div class="col-6 col-md-3 mb-4 box_image">
+                <a href="">
+                    <img src="/images/gallery/1.jpg" class="img-fluid"  />
+                </a>
+
+            </div>
+            <div class="col-6 col-md-3 mb-4 box_image">
+                <a href="">
+                    <img src="/images/gallery/1.jpg" class="img-fluid"  />
+                </a>
+
+            </div>
+            <div class="col-6 col-md-3 mb-4 box_image">
+                <a href="">
+                    <img src="/images/gallery/1.jpg" class="img-fluid"  />
+                </a>
+
+            </div>
+            <div class="col-6 col-md-3 mb-4 box_image">
+                <a href="">
+                    <img src="/images/gallery/1.jpg" class="img-fluid"  />
+                </a>
+
+            </div>
+            <div class="col-6 col-md-3 mb-4 box_image">
+                <a href="">
+                    <img src="/images/gallery/1.jpg" class="img-fluid"  />
+                </a>
+
+            </div>
+            <div class="col-6 col-md-3 mb-4 box_image">
+                <a href="">
+                    <img src="/images/gallery/2.jpg" class="img-fluid"  />
+                </a>
+
+            </div>
+            <div class="col-6 col-md-3 mb-4 box_image">
+                <a href="">
+                    <img src="/images/gallery/2.jpg" class="img-fluid"  />
+                </a>
+
+            </div>
+            <div class="col-6 col-md-3 mb-4 box_image">
+                <a href="">
+                    <img src="/images/gallery/1.jpg" class="img-fluid"  />
+                </a>
+
+            </div>
+            <div class="col-6 col-md-3 mb-4 box_image">
+                <a href="">
+                    <img src="/images/gallery/2.jpg" class="img-fluid"  />
+                </a>
+
+            </div>
+
+        </div>
+    </div>
+
+</main>
+<footer class="container" dir="rtl">
+    <div class="row pt-4">
+
+        <div class="col-md-3">
+            <b class="d-block">پیوندها</b>
+            <a href="" class="d-block">وب سایت آستان قدس رضوی</a>
+            <a href="" class="d-block">وب سایت حرم مطهر</a>
+            <a href="" class="d-block">سایت شفافیت بنیاد بهره وری موقوفات</a>
+            <a href="" class="d-block">سایت شفافیت خبری آستان قدس</a>
+            <a href="" class="d-block">سایت سیاست های کلان آستان قدس رضوی</a>
+        </div>
+        <div class="col-md-3">
+            <b class="d-block">ارتباط با ما</b>
+            <p class="m-0 text-justify">مشهد . چهارراه شهدا . سازمان مرکزی آستان قدس رضوی . معاونت طرح و برنامه 051-3225058 . 051-32001146 </p>
+            <a href="mailto:tarh.bamame@aqrazavi.org">tarh.bamame@aqrazavi.org</a>
+        </div>
+        <div class="col-md-2 align-items-center text-justify">
+            <b>&nbsp;</b>
+            <p>تمام حقوق این وبگاه متعلق به آستان قدس رضوی است</p>
+        </div>
+        <div class="col-md-2">
+            <b class="d-block">لینک ها</b>
+            <a href="" class="d-block">اخبار و اطلاعیه</a>
+            <a href="" class="d-block">ارتباط با ما</a>
+            <a href="" class="d-block">آشنایی با آستان قدس رضوی</a>
+            <a href="" class="d-block">سوالات متداول</a>
+            <a href="" class="d-block">راهنمای اصلاحات سامانه</a>
+        </div>
+        <div class="col-md-2">
+            <img src="/images/logo_aq.png" class="img-fluid" />
+        </div>
+
+    </div>
+</footer>
+
+<script src="/js/jquery.slim.min.js" ></script>
+<script src="/js/bootstrap.bundle.min.js" ></script>
+</body>
+</html>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="/css/bootstrap.min.css" />
+    <link href="/css/bootstrap-rtl.min.css" rel="stylesheet" />
+    <link href="/css/style.css" rel="stylesheet" />
+    <title></title>
+</head>
+<body>
+<main class="container-fluid" id="main_home" dir="rtl">
+    <div class="row align-items-center h-100">
+        <div class="col-md-4 text-right mx-auto d-none d-md-block d-lg-block d-xl-block">
+            <div class="row" dir="ltr">
+                <div class="col-8  border-left  border-bottom p-5"></div>
+            </div>
+            <a href="/farsi/home">نخستین جشنواره بین المللی عکس مزارات</a>
+            <div class="row" dir="ltr">
+                <div class="col-8   p-5"></div>
+            </div>
+        </div>
+        <div class="col-md-4 text-center mx-auto">
+            <img src="/images/logo_green.png" class="img-fluid" />
+        </div>
+        <div class="col-md-4 text-left mx-auto">
+            <div class="row" dir="ltr">
+                <div class="col-8   p-5"></div>
+            </div>
+            <a href="">Mazaarat International Photo Festival 2021</a>
+            <div class="row" dir="rtl">
+                <div class="col-8  border-right  border-top p-5"></div>
+            </div>
+        </div>
+    </div>
+</main>
+
+<script src="/js/jquery.slim.min.js" ></script>
+<script src="/js/bootstrap.bundle.min.js" ></script>
+</body>
+</html>
