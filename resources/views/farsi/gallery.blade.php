853017ecd0764cc7bc1096512296277e65870d59
Yaser Bahrami <yaser.bahrami@gmail.com>
2023-04-25 03:50:49 +0430

    fix gallery

--- resources/views/farsi/gallery.blade.php
+++ resources/views/farsi/gallery.blade.php
@@ -3,76 +3,75 @@
 @section('content')
     <div class="container">
         <div class="row align-items-center mt-3 mb-3 gallery" id="gallery">
-            <div class="col-6 col-md-3 mb-4 box_image">
+            <div class="col-12 col-md-4 col-lg-3 mb-4 box_image">
                 <a href="/images/gallery/mazaar/13-11.jpg" rel="rel1" data-caption="" >
                     <img src="/images/gallery/mazaar/13-11.jpg" class="h-100"  />
                 </a>
             </div>
-            <div class="col-6 col-md-3 mb-4 box_image">
+            <div class="col-12 col-md-4 col-lg-3 mb-4 box_image">
                 <a href="/images/gallery/mazaar/13-13.jpg" rel="rel1"  >
                     <img src="/images/gallery/mazaar/13-13.jpg" class="h-100"  />
                 </a>
 
             </div>
-            <div class="col-6 col-md-3 mb-4 box_image">
+            <div class="col-12 col-md-4 col-lg-3 mb-4 box_image">
                 <a href="/images/gallery/mazaar/27-12.jpg" rel="rel1"  >
                     <img src="/images/gallery/mazaar/27-12.jpg" class="h-100"  />
                 </a>
 
             </div>
-            <div class="col-6 col-md-3 mb-4 box_image">
+            <div class="col-12 col-md-4 col-lg-3 mb-4 box_image">
                 <a href="/images/gallery/mazaar/43-13.jpg" rel="rel1"  >
                     <img src="/images/gallery/mazaar/43-13.jpg" class="h-100"  />
                 </a>
 
             </div>
-            <div class="col-6 col-md-3 mb-4 box_image">
+            <div class="col-12 col-md-4 col-lg-3 mb-4 box_image">
                 <a href="/images/gallery/mazaar/44-14.jpg" rel="rel1"  >
                     <img src="/images/gallery/mazaar/44-14.jpg" class="h-100"  />
                 </a>
 
             </div>
-            <div class="col-6 col-md-3 mb-4 box_image">
+            <div class="col-12 col-md-4 col-lg-3 mb-4 box_image">
                 <a href="/images/gallery/mazaar/86-13.jpg" rel="rel1"  >
                     <img src="/images/gallery/mazaar/86-13.jpg" class="h-100"  />
                 </a>
 
             </div>
-            <div class="col-6 col-md-3 mb-4 box_image">
+            <div class="col-12 col-md-4 col-lg-3 mb-4 box_image">
                 <a href="/images/gallery/mazaar/94-11.jpg" rel="rel1"  >
                     <img src="/images/gallery/mazaar/94-11.jpg" class="h-100"  />
                 </a>
 
             </div>
-            <div class="col-6 col-md-3 mb-4 box_image">
+            <div class="col-12 col-md-4 col-lg-3 mb-4 box_image">
                 <a href="/images/gallery/mazaar/161-11.jpg" rel="rel1"  >
                     <img src="/images/gallery/mazaar/161-11.jpg" class="h-100"  />
                 </a>
 
             </div>
-            <div class="col-6 col-md-3 mb-4 box_image">
+            <div class="col-12 col-md-4 col-lg-3 mb-4 box_image">
                 <a href="/images/gallery/mazaar/179-11.jpg" rel="rel1"  >
                     <img src="/images/gallery/mazaar/179-11.jpg" class="h-100"  />
                 </a>
 
             </div>
-            <div class="col-6 col-md-3 mb-4 box_image">
+            <div class="col-12 col-md-4 col-lg-3 mb-4 box_image">
                 <a href="/images/gallery/mazaar/246-11.jpg" rel="rel1"  >
                     <img src="/images/gallery/mazaar/246-11.jpg" class="h-100"  />
                 </a>
 
             </div>
-            <div class="col-6 col-md-3 mb-4 box_image">
+            <div class="col-12 col-md-4 col-lg-3 mb-4 box_image">
                 <a href="/images/gallery/mazaar/260-13.jpg" rel="rel1"  >
                     <img src="/images/gallery/mazaar/260-13.jpg" class="h-100"  />
                 </a>
 
             </div>
-            <div class="col-6 col-md-3 mb-4 box_image">
+            <div class="col-12 col-md-4 col-lg-3 mb-4 box_image">
                 <a href="/images/gallery/mazaar/277-14.jpg">
                     <img src="/images/gallery/mazaar/277-14.jpg" class="h-100"  />
                 </a>
-
             </div>
 
         </div>
