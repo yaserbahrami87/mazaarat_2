0e95ed270fbcfbac23cc27b9c08810343e02f5db
Yaser Bahrami <yaser.bahrami@gmail.com>
2023-04-26 01:37:03 +0430

    chnage font

--- resources/views/farsi/master/footer.blade.php
+++ resources/views/farsi/master/footer.blade.php
@@ -2,34 +2,35 @@
     <div class="col-12 text-center">
         <img src="/images/footer.png" width="60px" class="position-relative" id="bolt-footer"/>
     </div>
-    <div class="row pt-4">
+    <div class="row pt-4  mb-3">
 
         <div class="col-md-4">
             <b class="d-block">ارتباط با ما</b>
-            <p class="m-0 text-justify">مشـهـد. خیـابـان کـوهـسنـگی.کوهسنـگی 11. عـدالـت 18. پـلاک 9.1
+            <p class="m-0 text-justify">مشـهـد مقدس. خیـابـان کـوهـسنـگی 11. عـدالـت 18. پـلاک 9
                 مـوسســــه آفــرینـش‌هــــای هـنـــــری آستـــــان قــــدس رضــــــوی</p>
-            <a href="tel:05132001146" class="mr-3">051-32001146</a>
-            <span class="text-light">.</span>
-            <a href="tel:05132001146" class="ml-3">051-3225058</a>
-            <a href="mailto:tarh.bamame@aqrazavi.org" class="d-block">tarh.bamame@aqrazavi.org</a>
+            <a href="tel:09390482732" class="mr-3">09390482732</a>
+            <a href="mailto:info@mazaar.net" class="d-block">info@mazaar.net</a>
         </div>
         <div class="col-md-2 align-items-center text-justify">
-            <b>&nbsp;</b>
-            <p>تمام حقــوق این وبـگاه متعلق به آستان قدس رضوی است</p>
+
+            <p>مالکیت حقوقی و معنوی این تارنما متعلق به دبیرخانه جشنواره بین المللی عکس مزارات می باشد</p>
         </div>
-        <div class="col-md-3 align-items-center text-center">
+        <div class="col-md-2 align-items-center text-center p-0">
             <a href="https://www.facebook.com/mazaar.photo/" target="_blank" class="d-inline-block social mr-3" id="facebook_footer"></a>
             <a href="https://t.me/Mazaarat_2023" target="_blank" class="d-inline-block social mr-3" id="telegram_footer"></a>
             <a href="" class="d-inline-block social mr-3" id="insta_footer"></a>
             <a href="" class="d-inline-block social mr-3" id="whatsapp_footer"></a>
         </div>
-        <div class="col-md-3">
+        <div class="col-md-4">
             <a href="https://www.aqart.ir/Maktab-Rezvan.aspx" target="_blank">
-                <img src="/images/logo_maktab.png" width="120px" class="mr-2" />
+                <img src="/images/logo_maktab.png" width="100px" class="mr-2" />
             </a>
 
             <a href="https://www.aqart.ir/" target="_blank">
-                <img src="/images/logo_aq.png" width="120px" />
+                <img src="/images/logo_aq.png" width="100px" />
+            </a>
+            <a href="https://www.aqart.ir/" target="_blank">
+                <img src="/images/logo_moavenat.png" width="100px" />
             </a>
 
         </div>
