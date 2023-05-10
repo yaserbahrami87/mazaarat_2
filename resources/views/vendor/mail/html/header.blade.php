61e4bd9f70a303e0ba91586f8ffd208e85e6493e
Yaser Bahrami <yaser.bahrami@gmail.com>
2023-05-11 03:28:43 +0430

    fix bug

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
+@if (trim($slot) === 'Mazaar')
 <img src="https://photo.mazaar.net/images/logo_green.png" class="logo" alt="Mazaarat">
 @else
 {{ $slot }}
