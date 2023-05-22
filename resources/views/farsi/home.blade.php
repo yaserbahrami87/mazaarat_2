86dd19e5f01825289d71b7fa4b346e4fa93c920a
Yaser Bahrami <yaser.bahrami@gmail.com>
2023-05-22 11:36:24 +0430

    remove banner

--- resources/views/farsi/home.blade.php
+++ resources/views/farsi/home.blade.php
@@ -12,9 +12,7 @@
                         <div class="carousel-item">
                             <img src="/images/banner-02.jpg" class="d-block w-100" alt="...">
                         </div>
-                        <div class="carousel-item">
-                            <img src="/images/banner1.jpg" class="d-block w-100" alt="...">
-                        </div>
+
                     </div>
                     <button class="carousel-control-prev" type="button" data-target="#carouselExampleControls" data-slide="prev">
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
