8862d0687e3822a521c9901ed8497add913f9bbb
Yaser Bahrami <yaser.bahrami@gmail.com>
2024-01-02 11:48:27 +0330

    add setting

--- resources/views/admin/master/rightNavbar.blade.php
+++ resources/views/admin/master/rightNavbar.blade.php
@@ -2,7 +2,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="/" class="brand-link">
-        <img src="/dist/img/AdminLTELogo.png" alt="مزارات" class="brand-image img-circle elevation-3"
+        <img src="/images/{{$settings->where('setting','logo')->first()['value']}}" alt="مزارات" class="brand-image img-circle elevation-3"
              style="opacity: .8">
         <span class="brand-text font-weight-light">پنل مدیریت</span>
     </a>
@@ -187,39 +187,19 @@
                                     <p>پوسترهای صفحه اول</p>
                                 </a>
                             </li>
+                            <!--
                             <li class="nav-item">
                                 <a href="/admin/setting/colleagues" class="nav-link">
                                     <i class="fa fa-circle-o nav-icon"></i>
                                     <p>شرکا/همکاران</p>
                                 </a>
                             </li>
+                            -->
 
                         </ul>
                     </li>
 
-                    <li class="nav-item has-treeview">
-                        <a href="#" class="nav-link ">
-                            <i class="nav-icon fa fa-"></i>
-                            <p>
-                                تنظیمات
-                                <i class="right fa fa-angle-left"></i>
-                            </p>
-                        </a>
-                        <ul class="nav nav-treeview">
-                            <li class="nav-item">
-                                <a href="/admin/setting/slider" class="nav-link">
-                                    <i class="fa fa-circle-o nav-icon"></i>
-                                    <p>تنظیمات اسلایدر صفحه اول</p>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="/admin/gallery/create" class="nav-link">
-                                    <i class="fa fa-circle-o nav-icon"></i>
-                                    <p>اضافه کردن عکس</p>
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
+
                     <li class="nav-item has-treeview">
                         <a href="#" class="nav-link ">
                             <i class="nav-icon fa fa-email"></i>
